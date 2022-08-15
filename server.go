8b01c5976639f9962ddad4b77ac0bf43edb35ee8
jay-bulk <67493409+jay-bulk@users.noreply.github.com>
2022-08-15 13:39:03 -0600

    update server.go
    pushing to main

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package main
 
 
-import "net/http"
+import (
+  "net/http"
+  "encoding/json"
+)
 
 type Tester struct {
   Name string `json:"name"`
@@ -15,12 +18,37 @@ type testHandlers struct {
   store map[string]Tester
 }
 func (h *testHandlers) get(w http.ResponseWriter, r *http.Request) {
-
+  testers := make([]Tester, len(h.store))
+  i := 0
+  for _, tester := range h.store {
+    testers[i] = tester
+    i++
+  }
+  jsonBytes, err := json.Marshal(testers)
+  if err != nil {
+    // TODO
+  }
+  w.Write(jsonBytes)
 }
 
 func newTestHandlers() *testHandlers{
   return &testHandlers {
-    store: map[string]Tester{},
+    store: map[string]Tester{
+      "id1": Tester{
+        Name: "John Doe",
+        Station: 89,
+        ID: "113213",
+        Relation: "founderson",
+        Job: "technician",
+      },
+      "id2": Tester{
+        Name: "exzibit",
+        Station: 22,
+        ID: "1",
+        Relation: "none",
+        Job: "entertainer",
+      },
+    },
   }
 }
 
