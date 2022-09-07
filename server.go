3f6abebffe666296839b5329d85ec3dee6f13f78
jay-bulk <67493409+jay-bulk@users.noreply.github.com>
2022-09-07 15:23:24 -0600

    auth and id response
    adding basic auth and single id response method

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
   "io/ioutil"
   "fmt"
   "time"
+  "os"
+  "strings"
 )
 
 //This is a "zero" dependency api
@@ -57,22 +59,47 @@ func (h *testHandlers) get(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
   w.Write(jsonBytes)
 }
+func (h *testHandlers) getTester(w http.ResponseWriter, r *http.Request) {
+  parts := strings.Split(r.URL.String(), "/")
+  if len(parts) != 3 {
+    w.WriteHeader(http.StatusNotFound)
+    return
+  }
+  
+  h.Lock()
+  tester, ok := h.store[parts[2]]
+  h.Unlock()
+  if !ok {
+    w.WriteHeader(http.StatusNotFound)
+    return
+  }  
+    jsonByters, err := json.Marshal(tester)
+    if err != nil {
+      w.WriteHeader(http.StatusInternalServerError)
+      w.Write([]byte(err.Error()))
+    }
+    w.Header().Add("content-type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonByters)
+}
+
 func (h *testHandlers) post(w http.ResponseWriter, r *http.Request) {
   bodyBytes, err := ioutil.ReadAll(r.Body)
   defer r.Body.Close()
   if err != nil {
      w.WriteHeader(http.StatusInternalServerError)
      w.Write([]byte(err.Error()))
+     return
   }
 
   ct := r.Header.Get("content-type")
   if ct != "application/json" {
      w.WriteHeader(http.StatusUnsupportedMediaType)
-     w.Write([]byte(fmt.Sprintf("need content-type 'application/json', not got '%s'", ct)))
+     w.Write([]byte(fmt.Sprintf("need content-type 'application/json', but got '%s'", ct)))
      return
   }
    
-  }
+  
   var tester Tester
   err = json.Unmarshal(bodyBytes, &tester)
   if err != nil {
@@ -89,28 +116,39 @@ func (h *testHandlers) post(w http.ResponseWriter, r *http.Request) {
 
 func newTestHandlers() *testHandlers{
   return &testHandlers {
-    store: map[string]Tester{
-      "id1": Tester{
-        Name: "John Doe",
-        Station: 89,
-        ID: "113213",
-        Relation: "founderson",
-        Job: "technician",
-      },
-      "id2": Tester{
-        Name: "exzibit",
-        Station: 22,
-        ID: "1",
-        Relation: "none",
-        Job: "entertainer",
-      },
-    },
+    store: map[string]Tester{ },
   }
 }
 
+type adminPortal struct {
+  password string
+}
+
+func newAdminPortal() *adminPortal {
+  password := os.Getenv("ADMIN_PASSWORD")
+  if password == "" {
+    panic("required env var ADMIN_PASSWORD not set")
+  }
+  return &adminPortal{password: password}
+}
+
+func (a adminPortal) handler(w http.ResponseWriter, r *http.Request) {
+  user, pass, ok := r.BasicAuth()
+  if !ok || user != "admin" || pass != a.password {
+    w.WriteHeader(http.StatusUnauthorized)
+    w.Write([]byte("401 - unauthorized"))
+    return
+  }
+  w.Write([]byte("<html><h1>Super Secret admin portal</h1></html>"))
+  
+}
+
 func main() {
+  admin := newAdminPortal()
   testHandlers := newTestHandlers()
   http.HandleFunc("/testers", testHandlers.testers)
+  http.HandleFunc("/testers/", testHandlers.getTester)
+  http.HandleFunc("/admin", admin.handler)
   err := http.ListenAndServe(":8080", nil)
   if err != nil {
     panic(err)
